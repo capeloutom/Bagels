7a156506df7bbf015c8e4dd54e00c828be7bfbfa
capeloutom <capeloutom@cmc102-06.mathcs.carleton.edu>
2017-11-14 17:01:12 -0600

    Idiot proofed the guessing mode of the game

--- BagelProject.py
+++ BagelProject.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 def Rules():
 	print("The game will begin with you either choosing to be the guesser or the one \
 	generating the number.  The person generating the number will choose a 3 digit number with no \
@@ -10,8 +12,11 @@ def Rules():
 	guess numbers until a correct number is found. TEST")
 
 def startGame():
-	gamemode = input("Would you like to be the player guessing the number or creating the number? \
-		please input 'guessing' or 'creating' " )
+	gamemode = 0
+	while gamemode != "guessing" and gamemode != "creating":
+		gamemode = input("Would you like to be the player guessing the number or creating the number? \
+			please input 'guessing' or 'creating'\n " )
+	return gamemode
 
 def generateNum():
 	number1 = random.randint(1,9) 
@@ -29,42 +34,67 @@ def generateNum():
 	return numtoGuess
 
 def guessing(numtoGuess):
-	playerGuess = []
+	playerGuess = [] #guess starts empty
+	digits = ["0","1","2","3","4","5","6","7","8","9"] #list of digits so we can make sure guess is a number
 	while playerGuess != numtoGuess:
-		playerGuess = []
-		response = []
+		playerGuess = [0,0,0]
 		print(numtoGuess)
-		guessString = input("Guess a 3 digit number: ")
-		for ch in guessString:
-			playerGuess.append(int(ch))
+		response = [] #starts blank until guess is made
+
+		#ensure that guess has no repeated digit
+		while playerGuess[0] in playerGuess[1:] or playerGuess[1] == playerGuess[2]:	
+			guessString = input("Guess a 3 digit number with no spaces in between numbers and no repeats: ")
+			
+			#check for characters other than number in guess
+			while guessString[0] not in digits \
+			or guessString[1] not in digits or guessString[2] not in digits:
+				print("please make a guess using only the numbers 0-9")
+				guessString = input("Guess a 3 digit number with no spaces in between numbers and no repeats: ")
+			
+			#makes sure guess is 3 digits long
+			while len(guessString) != 3:
+				print("please make a guess only 3 digits in length")
+				guessString = input("Guess a 3 digit number with no spaces in between numbers and no repeats: ")
+			
+			temp = []
+			
+			for ch in guessString:
+				temp.append(int(ch)) #makes a list with guess
+			
+			for i in range(len(temp)):
+				playerGuess[i] = temp[i] #makes player guess = to new guess
+
+
 
 		if playerGuess[0] not in numtoGuess: #this code is for bagels
 			if playerGuess[1] not in numtoGuess and playerGuess[2] not in numtoGuess:
 				response.append("Bagel")
 			else: 
 					pass
-		else: 
-			pass
+		
 
-		#code for Pico
+		#code for Pico / Fermi
 		for ch in range(len(playerGuess)):
 			if playerGuess[ch] in numtoGuess:
 				if playerGuess[ch] == numtoGuess[ch]:
-					response.insert(4, "Fermi")
+					response.insert(4, "Fermi") #inserted at 4 to ensure Fermis always come last
 				else:
-						response.insert(0, "Pico")
+						response.insert(0, "Pico") #inserted at 0 to ensure Pico's come first
 		print(response)
+	print("Congradulations, you guessed the correct number!")
 
-
-
-
-
+def creating():
+	print("The computer will make a guess then you will input the correct response \
+		if you input fermi fermi fermi, then the guess is correct and the game will be over")
+	
 
 def main():
-	# if gamemode == "guessing":
-	# 	guessing()
-	test = generateNum()
-	guessing(test)
+	gamemode = startGame()
+	if gamemode == "guessing":
+		test = generateNum()
+		guessing(test)
+	if gamemode == "creating":
+		creating()
 
 if __name__ == "__main__":
 	main()
